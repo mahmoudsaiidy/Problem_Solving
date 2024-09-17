51d1f3e40be53ab42222d403450316576b365300
Mahmoud <90093238+Damahmoud@users.noreply.github.com>
2024-09-17 03:56:34 +0300

    Update dx.cpp

--- Grid/dx.cpp
+++ Grid/dx.cpp
@@ -1,2 +1,23 @@
+
+// king and queen
 int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+
+// knight  
+int nitx[] = { 2, 2, -2, -2, 1, 1, -1, -1 };
+int nity[] = { 1, -1, 1, -1, 2, -2, 2, -2 };
+
+// rook
+int rx[] = {1, -1, 0, 0};
+int ry[] = {0, 0, 1, -1};
+
+// bishop 
+int bx[] = {1, 1, -1, -1};
+int by[] = {1, -1, 1, -1};
+
+// pawn 
+int px[] = {1};   
+int py[] = {0};   
+
+int capture_px[] = {1, 1};  
+int capture_py[] = {1, -1};
